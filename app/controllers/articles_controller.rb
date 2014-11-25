0eb57d174eda8a752bb3bbde99d9a9e97ab284ab
deic1de <de1c1de2life@gmail.com>
2014-11-25 20:04:59 +0300

    final commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,22 @@
 class ArticlesController < ApplicationController
+  
+  http_basic_authenticate_with name: "poshel", password: "nahooi", except: [:index, :show]
+  
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
 
   def create
-    #@article = Article.new(article_params)
-      #if @article.save
-      #  redirect_to @article
-      #else
-      #  render 'new'
-      #end
-      render plain: params.inspect
+    @article = Article.new(article_params)
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
   end
   
   def edit
